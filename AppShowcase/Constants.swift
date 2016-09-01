2c87f6a4bab5d29a0d6fe096e3c629a630c99268
Jay Bhavsar <BlackHat@Jays-MacBook-Pro.local>
2016-09-01 12:09:54 -0400

    initial commit

---
+++ AppShowcase/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  AppShowcase
+//
+//  Created by Jay Bhavsar on 2016-08-29.
+//  Copyright © 2016 zeroxp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0/255.0
