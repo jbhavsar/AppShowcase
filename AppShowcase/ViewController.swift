d948def5a0e93ec1fc97cd3edbfb8a955898f53f
Jay Bhavsar <BlackHat@Jays-MacBook-Pro.local>
2016-09-07 18:06:37 -0400

    Facebook Login functionality added

--- AppShowcase/ViewController.swift
+++ AppShowcase/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
@@ -20,6 +22,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func fbBtnPressed(sender: UIButton!){
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logInWithReadPermissions(["email"]) { (facebookResult:FBSDKLoginManagerLoginResult!, facebookError: NSError!) -> Void in
+            
+            if facebookError != nil{
+                print ("Facebook login failed. Error \(facebookError)")
+            }
+            else{
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                print("Successfully logged in with Facebook \(accessToken)")
+            }
+        }
+        
+    }
+    
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return UIStatusBarStyle.LightContent
     }
